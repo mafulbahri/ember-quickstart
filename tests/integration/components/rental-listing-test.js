d576d27c8c65b36a7c819c2838a41521857d0a7f
Mohamad Maful Bahri <mohamad.mafulbahri@mitrais.com>
2017-01-08 12:38:34 +0700

    Add integration test for rental-listing component

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('rental-listing', 'Integration | Component | rental listing', {
   integration: true
@@ -22,4 +23,23 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('should toggle wide class on click', function(assert) {
+  assert.expect(3);
+  let stubRental = Ember.Object.create({
+    image: 'fake.png',
+    title: 'test-title',
+    owner: 'test-owner',
+    type: 'test-type',
+    city: 'test-city',
+    bedrooms: 3
+  });
+  this.set('rentalObj', stubRental);
+  this.render(hbs`{{rental-listing rental=rentalObj}}`);
+  assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
+  this.$('.image').click();
+  assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
+  this.$('.image').click();
+  assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
 });
