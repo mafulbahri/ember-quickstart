d27defd35c5b0a9ac8ac674bea74cf5988948f31
Mohamad Maful Bahri <mohamad.mafulbahri@mitrais.com>
2017-01-08 13:17:00 +0700

    Update rental-property-type helper and use it in rental-listing - this helper works like filter in angularjs, e.g formats the view in the template

--- app/helpers/rental-property-type.js
+++ app/helpers/rental-property-type.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 
-export function rentalPropertyType(params/*, hash*/) {
-  return params;
+const communityPropertyTypes = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
+/*
+ * Handlebars passes an array of arguments from our template to our helper.
+ * We are using ES2015 destructuring to get the first item in the array and name it 'type'.
+ * Then we can check to see if type exists in our communityPropertyTypes array.
+ */
+export function rentalPropertyType([type]/*, hash*/) {
+  if (communityPropertyTypes.includes(type)) {
+    return 'Community';
+  }
+
+  return 'Standalone';
 }
 
 export default Ember.Helper.helper(rentalPropertyType);
