d410676a016341f5b6a67940ad5498ed16ef1674
Mohamad Maful Bahri <mohamad.mafulbahri@mitrais.com>
2017-01-09 05:42:37 +0700

    Update maps service unit test

--- tests/unit/services/maps-test.js
+++ tests/unit/services/maps-test.js
@@ -1,12 +1,37 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const DUMMY_ELEMENT = {};
+
+let MapsUtilStub = Ember.Object.extend({
+  createMap(element, location) {
+    this.assert.ok(element, 'createMap called with element');
+    this.assert.ok(location, 'createMap called with location');
+    return DUMMY_ELEMENT;
+  }
+});
 
 moduleFor('service:maps', 'Unit | Service | maps', {
   // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  needs: ['service:google-maps']
 });
 
 // Replace this with your real tests.
-test('it exists', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+test('should create a new map if one isn\'t cached for location', function(assert) {
+  assert.expect(4);
+  let stubMapUtil = MapsUtilStub.create({ assert });
+  let mapService = this.subject({ mapUtil: stubMapUtil });
+  let element = mapService.getMapElement('San Fransisco');
+  assert.ok(element, 'element exists');
+  assert.equal(element.className, 'map', 'element has class name of map');
+});
+
+test('should use existing map if one is cached for location', function(assert) {
+  assert.expect(1);
+  let stubCachedMaps = Ember.Object.create({
+    sanFransisco: DUMMY_ELEMENT
+  });
+  let mapService = this.subject({ cachedMaps: stubCachedMaps });
+  let element = mapService.getMapElement('San Fransisco');
+  assert.equal(element, DUMMY_ELEMENT, 'element fetched from cache');
 });
